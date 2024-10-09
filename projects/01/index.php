1a8f760e238bee21fdb3702e366600889f8553ce
brennanc14 <brennancoleman@mail.weber.edu>
2024-10-09 18:36:49 +0000

    created project 01

---
+++ projects/01/index.php
@@ -0,0 +1,9 @@
+<?php include 'templates/head.php'; ?>
+<?php include 'templates/nav.php'; ?>
+<?php include 'config.php'; ?>
+
+    <!-- BEGIN YOUR CONTENT -->
+
+    <!-- END YOUR CONTENT -->
+
+<?php include 'templates/footer.php'; ?>
